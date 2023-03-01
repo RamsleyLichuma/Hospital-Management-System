b26fc29136efbc65dabdca19ca4fe9fe3eac6047
Ramsley Lichuma <ramsleylichuma@gmail.com>
2023-03-01 09:37:29 +0300

    first

---
+++ dbconnection.php
@@ -0,0 +1,16 @@
+<?php
+    $user = "lichuma"; 
+    $password = "lichuma2023";
+    $ODBCConnection = odbc_connect(
+    "DRIVER={Devart ODBC Driver for SQL Server};
+    Server=DESKTOP-TQ5IM0U;
+    Database=hms; 
+    Port=8080;
+    String Types=Unicode", $user, $password);
+    $SQLQuery = "SELECT * FROM hms";
+    $RecordSet = odbc_exec($ODBCConnection, $SQLQuery);
+    while (odbc_fetch_row($RecordSet)) {
+        $result = odbc_result_all($RecordSet, "border=1");
+    }
+    odbc_close($ODBCConnection);
+?>  
